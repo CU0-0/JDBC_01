69e75717e1cc2763daef69637317c6a2d15695d3
CU0-0 <59238699+CU0-0@users.noreply.github.com>
2026-04-10 14:29:37 +0900

    Add files via upload

--- jdbc-workspace/00_JDBC_START/src/com/kh/start/run/InsertRun.java
+++ jdbc-workspace/00_JDBC_START/src/com/kh/start/run/InsertRun.java
@@ -40,18 +40,25 @@ public class InsertRun {
 		
 		// 사용자가 입력한 값을 DBMS로 보내서 => ZONE테이블에 INSERT
 		Scanner sc = new Scanner(System.in);
-		/*
 		System.out.println("ZONE_ID를 입력해주세요 > ");
 		String zoneId = sc.nextLine();
 		System.out.println("ZONE_NAME을 입력해주세요 > ");
 		String zoneName = sc.nextLine();
 		System.out.println("ZONE_TYPE을 입력해주세요 > ");
 		String zoneType = sc.nextLine();
-		*/
+		
 		String sql = "INSERT INTO ZONE VALUES('50', '테스트', '테스트')";
+		sql = "INSERT INTO ZONE VALUES('" + zoneId + "',"
+								   + "'"  + zoneName + "',"
+								   + "'"  + zoneType + "')";
+		StringBuilder sb = new StringBuilder();
+		sb.append("INSERT INTO ZONE VALUES(");
+		sb.append("'" + zoneId + "',");
+		sb.append("'" + zoneName + "',");
+		sb.append("'" + zoneType + "')");
+		sql = sb.toString();
 		
 		// 나는 오라클 DBMS에다가 작업할꺼다!
-		
 		try {
 			// 리플렉션을 사용해서 동적으로 클래스 로드
 			// 1) JDBC Driver 등록 -> ORACLE -> ojdbc.jar
@@ -59,12 +66,6 @@ public class InsertRun {
 			System.out.println("Driver 등록성공");
 			
 			//2) DB서버랑 연결(IP주소, PORT번호, 사용자이름, 비밀번호)
-			/*
-			  DriverManager.getConnection("URL",
-										  "사용자이름" ,
-										  "비밀번호");
-			*/
-			
 			conn = DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:XE", 
 												"C##SU", 
 												"SU");
@@ -85,11 +86,17 @@ public class InsertRun {
 			if(result > 0) {
 				conn.commit();
 			}
-			
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
+			System.out.println("65행으로 가시오"); 
+			System.out.println("1. ojdbc.jar 파일을 추가하지 않음");
+			System.out.println("2. oracle.jdbc.driver.OracleDriver 오타확인");
 		}catch (SQLException e) {
 				e.printStackTrace();
+				System.out.println("URL을 잘못적은 경우 -> 69행 오타 확인");
+				System.out.println("username/password; logon denied -> 70행, 71행 오타확인");
+				System.out.println("SQLSyntaxErrorException -> SQL문 오타 확인");
+				System.out.println("NullPointerException -> JDBC객체들 확인하기");
 		}finally {
 			try {
 				stmt.close();
